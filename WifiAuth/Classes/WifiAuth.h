d96964cdb5973b423809241a875a44761ecae63f
Tim Johnsen <timonus@gmail.com>
2018-12-24 16:06:09 -0800

    Making some modifications.

--- WifiAuth/Classes/WifiAuth.h
+++ WifiAuth/Classes/WifiAuth.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WifiAuth : NSObject
 
+- (instancetype)init NS_UNAVAILABLE;
+
 + (instancetype)sharedWifiAuth;
 - (BOOL)startMonitoring;
 
--- WifiAuth/Classes/WifiAuth.m
+++ WifiAuth/Classes/WifiAuth.m
@@ -15,13 +15,12 @@
 #import <ifaddrs.h>
 #import <netdb.h>
 
-@interface WifiAuth()
+@interface WifiAuth ()
 
-@property (nonatomic, assign) Boolean currentlyShownPopup;
-@property (nonatomic, assign) Boolean offeredToShowPopup;
+@property (nonatomic, assign) BOOL currentlyShowingAlert;
 
-@property (nonatomic, assign) SCNetworkReachabilityRef  reachabilityRef;
-@property (nonatomic, strong) dispatch_queue_t          reachabilitySerialQueue;
+@property (nonatomic, assign) SCNetworkReachabilityRef reachabilityRef;
+@property (nonatomic, strong) dispatch_queue_t reachabilitySerialQueue;
 
 - (void)reachabilityChanged:(SCNetworkReachabilityFlags)flags;
 
@@ -35,23 +34,30 @@ static void WAReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
 
 @implementation WifiAuth
 
-+ (instancetype)sharedWifiAuth {
++ (instancetype)sharedWifiAuth
+{
     static dispatch_once_t once;
     static WifiAuth *sharedWifiAuth;
-    dispatch_once(&once, ^ { sharedWifiAuth = [[self alloc] init]; });
+    dispatch_once(&once, ^ { sharedWifiAuth = [[self alloc] initPrivate]; });
     return sharedWifiAuth;
 }
 
-- (BOOL)startMonitoring {
-    struct sockaddr_in zeroAddress;
-    bzero(&zeroAddress, sizeof(zeroAddress));
-    zeroAddress.sin_len = sizeof(zeroAddress);
-    zeroAddress.sin_family = AF_INET;
-    SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (const struct sockaddr*)&zeroAddress);
-    self.reachabilityRef = ref;
-    
-    self.reachabilitySerialQueue = dispatch_queue_create("fx.krause.wifiauth", NULL);
-    
+- (instancetype)initPrivate
+{
+    if (self = [super init]) {
+        struct sockaddr_in zeroAddress;
+        bzero(&zeroAddress, sizeof(zeroAddress));
+        zeroAddress.sin_len = sizeof(zeroAddress);
+        zeroAddress.sin_family = AF_INET;
+        SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (const struct sockaddr*)&zeroAddress);
+        self.reachabilityRef = ref;
+        self.reachabilitySerialQueue = dispatch_queue_create("fx.krause.wifiauth", NULL);
+    }
+    return self;
+}
+
+- (BOOL)startMonitoring
+{
     // Copped from Reachbility -startNotifier
     SCNetworkReachabilityContext context = { 0, NULL, NULL, NULL, NULL };
     if (SCNetworkReachabilitySetCallback(self.reachabilityRef, WAReachabilityCallback, &context)) {
@@ -65,46 +71,36 @@ static void WAReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
             return YES;
         }
     }
-    
     return NO;
 }
 
 - (void)reachabilityChanged:(SCNetworkReachabilityFlags)flags
 {
-    if (![self isInterventionRequiredWithFlags:flags]) {
-        return;
-    }
-    if (self.currentlyShownPopup) {
-        return;
-    }
-    if (self.offeredToShowPopup) {
+    if (self.currentlyShowingAlert || ![self isInterventionRequiredWithFlags:flags]) {
         return;
     }
     
-    // TODO: replace all those variables with other ones
-    self.currentlyShownPopup = YES;
-    self.offeredToShowPopup = YES;
+    self.currentlyShowingAlert = YES;
     
-    // Login here
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"WiFi Authentication needed"
-                                                                   message:@"Looks like the WiFi you're connected to requires some sort of login. Open the login page now?"
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Wi-Fi Authentication needed"
+                                                                   message:@"It looks like the Wi-Fi you're connected to requires some sort of login. Open the login page now?"
                                                             preferredStyle:UIAlertControllerStyleAlert];
     
     __weak __typeof(self) weakSelf = self;
-    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleCancel
-                                                          handler:^(UIAlertAction * action) {
-                                                              weakSelf.currentlyShownPopup = NO;
-                                                              
-                                                              // TODO: replace with in-app browser
-                                                              // In-app browser allows us to detect once the WiFi connection is established
-                                                              // And show the `Done` button
-                                                              NSURL *urlToOpen = [NSURL URLWithString:@"http://captive.apple.com/hotspot-detect.html"];
-                                                              [[UIApplication sharedApplication] openURL:urlToOpen];
-                                                          }];
-    UIAlertAction *noAction = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault
-                                                          handler:^(UIAlertAction * action) {
-                                                              weakSelf.currentlyShownPopup = NO;
-                                                          }];
+    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"Open login page" style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction * action) {
+                                                          weakSelf.currentlyShowingAlert = NO;
+                                                          
+                                                          // TODO: replace with in-app browser
+                                                          // In-app browser allows us to detect once the WiFi connection is established
+                                                          // And show the `Done` button
+                                                          NSURL *urlToOpen = [NSURL URLWithString:@"http://captive.apple.com/hotspot-detect.html"];
+                                                          [[UIApplication sharedApplication] openURL:urlToOpen];
+                                                      }];
+    UIAlertAction *noAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel
+                                                     handler:^(UIAlertAction * action) {
+                                                         weakSelf.currentlyShowingAlert = NO;
+                                                     }];
     
     [alert addAction:yesAction];
     [alert addAction:noAction];
@@ -112,7 +108,6 @@ static void WAReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:alert animated:YES completion:nil];
 
 }
-
 
 // Taken from https://github.com/tonymillion/Reachability
 // BSD licensed
